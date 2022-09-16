fbb407cb7d815680557b4562819e7aa332b9aa3b
Asker <kotsevasker0@gmail.com>
2022-09-16 22:43:34 +0300

    debag

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -3,16 +3,19 @@ import style from './homepage.module.css';
 import AuthHook from '../../hooks/AuthHook.js'
 
 import Alert from '@mui/material/Alert';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function HomePage() {
   const[alert,setAlert] = useState(true);
 
   const {isAuth,email} = AuthHook();
+  useEffect(()=>{
   setTimeout(()=>{
      setAlert(alert=>!alert)
   },2000)
+  },[])
+
   return isAuth? (
     <div className={style.homePage}>
 
